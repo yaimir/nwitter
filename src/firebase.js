22820742468c30595e9a5214ac95ecfd562cc8c0
yaimir <solongus.gmail.com>
2022-07-04 16:06:53 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB4qGiAQ6m-F-Y9KQFtP368HVPs8c3bBiQ",
+    authDomain: "nwitter-3ce57.firebaseapp.com",
+    projectId: "nwitter-3ce57",
+    storageBucket: "nwitter-3ce57.appspot.com",
+    messagingSenderId: "739737557995",
+    appId: "1:739737557995:web:19ff7f8361fe1a800057ae"
+};
+
+export default firebase.initializeApp(firebaseConfig);
